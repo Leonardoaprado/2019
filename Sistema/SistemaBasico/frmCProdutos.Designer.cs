0625b7fcf332436e5929777f46d7132426ddf50c
leonardoaprado <leonardo.aprado@hotmail.com>
2019-03-30 19:50:20 -0300

    codigo de cadastro de produtos pronto - corrigir erro em calculo

--- Sistema/SistemaBasico/frmCProdutos.Designer.cs
+++ Sistema/SistemaBasico/frmCProdutos.Designer.cs
@@ -28,21 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCProdutos));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lblQuantidade = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.lblFabricante = new System.Windows.Forms.Label();
+            this.txtFabricante = new System.Windows.Forms.TextBox();
+            this.lblLocalizacao = new System.Windows.Forms.Label();
+            this.lblUnidade = new System.Windows.Forms.Label();
+            this.txtUnidade = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLocalizacao = new System.Windows.Forms.TextBox();
+            this.txtValorUnitario = new System.Windows.Forms.TextBox();
+            this.lblValorUn = new System.Windows.Forms.Label();
+            this.txtValorTotal = new System.Windows.Forms.TextBox();
+            this.lblValorTotal = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.txtValidade = new System.Windows.Forms.TextBox();
+            this.lblVaidade = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(12, 21);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(275, 24);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "CADASTRO DE PRODUTOS";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(16, 11);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Codigo:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(19, 27);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(105, 20);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.Location = new System.Drawing.Point(136, 11);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(58, 13);
+            this.lblDescricao.TabIndex = 2;
+            this.lblDescricao.Text = "Descrição:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(139, 27);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(330, 20);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // lblQuantidade
+            // 
+            this.lblQuantidade.AutoSize = true;
+            this.lblQuantidade.Location = new System.Drawing.Point(16, 60);
+            this.lblQuantidade.Name = "lblQuantidade";
+            this.lblQuantidade.Size = new System.Drawing.Size(65, 13);
+            this.lblQuantidade.TabIndex = 4;
+            this.lblQuantidade.Text = "Quantidade:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(19, 76);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(105, 20);
+            this.txtQuantidade.TabIndex = 5;
+            // 
+            // lblFabricante
+            // 
+            this.lblFabricante.AutoSize = true;
+            this.lblFabricante.Location = new System.Drawing.Point(136, 60);
+            this.lblFabricante.Name = "lblFabricante";
+            this.lblFabricante.Size = new System.Drawing.Size(60, 13);
+            this.lblFabricante.TabIndex = 6;
+            this.lblFabricante.Text = "Fabricante:";
+            // 
+            // txtFabricante
+            // 
+            this.txtFabricante.Location = new System.Drawing.Point(139, 76);
+            this.txtFabricante.Name = "txtFabricante";
+            this.txtFabricante.Size = new System.Drawing.Size(330, 20);
+            this.txtFabricante.TabIndex = 7;
+            // 
+            // lblLocalizacao
+            // 
+            this.lblLocalizacao.AutoSize = true;
+            this.lblLocalizacao.Location = new System.Drawing.Point(16, 109);
+            this.lblLocalizacao.Name = "lblLocalizacao";
+            this.lblLocalizacao.Size = new System.Drawing.Size(109, 13);
+            this.lblLocalizacao.TabIndex = 8;
+            this.lblLocalizacao.Text = "Localização Estoque:";
+            // 
+            // lblUnidade
+            // 
+            this.lblUnidade.AutoSize = true;
+            this.lblUnidade.Location = new System.Drawing.Point(136, 109);
+            this.lblUnidade.Name = "lblUnidade";
+            this.lblUnidade.Size = new System.Drawing.Size(50, 13);
+            this.lblUnidade.TabIndex = 10;
+            this.lblUnidade.Text = "Unidade:";
+            // 
+            // txtUnidade
+            // 
+            this.txtUnidade.Location = new System.Drawing.Point(139, 125);
+            this.txtUnidade.Name = "txtUnidade";
+            this.txtUnidade.Size = new System.Drawing.Size(105, 20);
+            this.txtUnidade.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txtValidade);
+            this.panel1.Controls.Add(this.lblVaidade);
+            this.panel1.Controls.Add(this.btnCadastrar);
+            this.panel1.Controls.Add(this.txtValorTotal);
+            this.panel1.Controls.Add(this.lblValorTotal);
+            this.panel1.Controls.Add(this.txtValorUnitario);
+            this.panel1.Controls.Add(this.lblValorUn);
+            this.panel1.Controls.Add(this.txtUnidade);
+            this.panel1.Controls.Add(this.lblUnidade);
+            this.panel1.Controls.Add(this.txtLocalizacao);
+            this.panel1.Controls.Add(this.lblLocalizacao);
+            this.panel1.Controls.Add(this.txtFabricante);
+            this.panel1.Controls.Add(this.lblFabricante);
+            this.panel1.Controls.Add(this.txtQuantidade);
+            this.panel1.Controls.Add(this.lblQuantidade);
+            this.panel1.Controls.Add(this.txtDescricao);
+            this.panel1.Controls.Add(this.lblDescricao);
+            this.panel1.Controls.Add(this.txtCodigo);
+            this.panel1.Controls.Add(this.lblCodigo);
+            this.panel1.Location = new System.Drawing.Point(12, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(487, 258);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtLocalizacao
+            // 
+            this.txtLocalizacao.Location = new System.Drawing.Point(19, 125);
+            this.txtLocalizacao.Name = "txtLocalizacao";
+            this.txtLocalizacao.Size = new System.Drawing.Size(105, 20);
+            this.txtLocalizacao.TabIndex = 9;
+            // 
+            // txtValorUnitario
+            // 
+            this.txtValorUnitario.Location = new System.Drawing.Point(139, 175);
+            this.txtValorUnitario.Name = "txtValorUnitario";
+            this.txtValorUnitario.Size = new System.Drawing.Size(105, 20);
+            this.txtValorUnitario.TabIndex = 13;
+            this.txtValorUnitario.TextChanged += new System.EventHandler(this.txtValorUnitario_TextChanged);
+            // 
+            // lblValorUn
+            // 
+            this.lblValorUn.AutoSize = true;
+            this.lblValorUn.Location = new System.Drawing.Point(25, 178);
+            this.lblValorUn.Name = "lblValorUn";
+            this.lblValorUn.Size = new System.Drawing.Size(96, 13);
+            this.lblValorUn.TabIndex = 12;
+            this.lblValorUn.Text = "Valor Unitario:   R$";
+            // 
+            // txtValorTotal
+            // 
+            this.txtValorTotal.Location = new System.Drawing.Point(139, 214);
+            this.txtValorTotal.Name = "txtValorTotal";
+            this.txtValorTotal.Size = new System.Drawing.Size(105, 20);
+            this.txtValorTotal.TabIndex = 15;
+            this.txtValorTotal.TextChanged += new System.EventHandler(this.txtValorTotal_TextChanged);
+            // 
+            // lblValorTotal
+            // 
+            this.lblValorTotal.AutoSize = true;
+            this.lblValorTotal.Location = new System.Drawing.Point(25, 217);
+            this.lblValorTotal.Name = "lblValorTotal";
+            this.lblValorTotal.Size = new System.Drawing.Size(96, 13);
+            this.lblValorTotal.TabIndex = 14;
+            this.lblValorTotal.Text = "Valor Total:       R$";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(261, 175);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(207, 57);
+            this.btnCadastrar.TabIndex = 16;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txtValidade
+            // 
+            this.txtValidade.Location = new System.Drawing.Point(261, 125);
+            this.txtValidade.Name = "txtValidade";
+            this.txtValidade.Size = new System.Drawing.Size(105, 20);
+            this.txtValidade.TabIndex = 12;
+            // 
+            // lblVaidade
+            // 
+            this.lblVaidade.AutoSize = true;
+            this.lblVaidade.Location = new System.Drawing.Point(258, 109);
+            this.lblVaidade.Name = "lblVaidade";
+            this.lblVaidade.Size = new System.Drawing.Size(51, 13);
+            this.lblVaidade.TabIndex = 17;
+            this.lblVaidade.Text = "Validade:";
             // 
             // frmCProdutos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(515, 325);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTitulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCProdutos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Produtos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label lblQuantidade;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label lblFabricante;
+        private System.Windows.Forms.TextBox txtFabricante;
+        private System.Windows.Forms.Label lblLocalizacao;
+        private System.Windows.Forms.Label lblUnidade;
+        private System.Windows.Forms.TextBox txtUnidade;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txtValorTotal;
+        private System.Windows.Forms.Label lblValorTotal;
+        private System.Windows.Forms.TextBox txtValorUnitario;
+        private System.Windows.Forms.Label lblValorUn;
+        private System.Windows.Forms.TextBox txtLocalizacao;
+        private System.Windows.Forms.TextBox txtValidade;
+        private System.Windows.Forms.Label lblVaidade;
     }
 }
--- Sistema/SistemaBasico/frmCProdutos.cs
+++ Sistema/SistemaBasico/frmCProdutos.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,75 @@ namespace SistemaBasico
             InitializeComponent();
         }
 
-        
+        private void txtValorTotal_TextChanged(object sender, EventArgs e)
+        {
+                        
+        }
+
+        private double Multiplicacao(int num1, int num2)
+        {
+            int total;
+            total = num1 * num2;
+            return total;
+        }
+
+        private void txtValorUnitario_TextChanged(object sender, EventArgs e)
+        {
+            txtValorTotal.Text = (Multiplicacao(Convert.ToInt32(txtValorUnitario.Text), Convert.ToInt32(txtQuantidade.Text))).ToString("N");
+            
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string Codigo, Descricao, Quantidade, Fabricante, Local, Unidade, Validade, ValorUnitario, ValorTotal;
+            Codigo = txtCodigo.Text;
+            Descricao = txtDescricao.Text;
+            Quantidade = txtQuantidade.Text;
+            Fabricante = txtFabricante.Text;
+            Local = txtLocalizacao.Text;
+            Unidade = txtUnidade.Text;
+            Validade = txtValidade.Text;
+            ValorUnitario = txtValorUnitario.Text;
+            ValorTotal = txtValorTotal.Text;
+
+            CadastrarProdutos(Codigo, Descricao, Quantidade, Fabricante, Local, Unidade, Validade, ValorUnitario, ValorTotal);
+            LimparCampos();
+
+        }
+
+        private void CadastrarProdutos(string Codigo, string Descricao, string Quantidade, string Fabricante, string Local, string Unidade, string Validade, string ValorUnitario, string ValorTotal)
+        {
+            StreamWriter arquivo;
+            string caminho = "C:\\sistema\\Produtos.txt";
+
+            arquivo = File.AppendText(caminho);
+            arquivo.WriteLine("Codigo: " + Codigo);
+            arquivo.WriteLine("Descrição: " + Descricao);
+            arquivo.WriteLine("Quantidade: " + Quantidade);
+            arquivo.WriteLine("Fabricante: " + Fabricante);
+            arquivo.WriteLine("Localização no Estoque: " + Local);
+            arquivo.WriteLine("Unidade: " + Unidade);
+            arquivo.WriteLine("Validade: " + Validade);
+            arquivo.WriteLine("Valor Unitario: " + ValorUnitario);
+            arquivo.WriteLine("Valor Total: " + ValorTotal);
+            arquivo.WriteLine("================================================");
+            arquivo.WriteLine();
+            arquivo.WriteLine();
+            arquivo.Close();
+        }
+
+        private void LimparCampos()
+        {
+            txtCodigo.Text = "";
+            txtDescricao.Text = "";
+            txtFabricante.Text = "";
+            txtLocalizacao.Text = "";
+            txtQuantidade.Text = "";
+            txtUnidade.Text = "";
+            txtValidade.Text = "";
+            txtValorTotal.Text = "";
+            txtValorUnitario.Text = "";
+
+        }
     }
 }
